02ff5a4302fd9aacb0c7ed626c0841fc5eb7576e
Thanawath Ruanson <toury1988@gmail.com>
2013-08-06 22:16:05 +0700

    recode evenet_manager

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,33 +1,47 @@
 require "csv"
-require "sunlight/congress"
-require "erb"
+require 'sunlight/congress'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 #should declare method on above
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-	Sunlight::Congress::legislators.by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
-puts "EventManager Initialized!"
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
 
-FILE = "event_attendees.csv"
+  filename = "output/thanks_#{id}.html"
 
-if File.exist?(FILE)
-	content = CSV.open FILE , :headers => true, :header_converters => :symbol
-  template_letter = File.read "form_letter.erb"
-	content.each do |row|
-	first_name = row[:first_name]
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
 
-	zipcode = clean_zipcode(row[:zipcode])
+puts "EventManager initialized."
 
-	legislators = legislators_by_zipcode(zipcode)
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
-	form_letter = erb_template.result(binding)
-	puts form_letter
-	end
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letters(id,form_letter)
 end
